3f8c0ca9aa866b9040e48b3dd28e0f4bc6b17da9
Caleb Njiiri <caleb.njiiri@gmail.com>
2020-07-15 22:15:37 +0300

    Implemented incoming webhooks

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from pprint import pprint
 import mailparser
 import html2markdown
 import configparser
-from slack import WebClient
+import requests
 import sched
+import json
 
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-BOT_TOKEN = config.get("SLACK", "BOT_TOKEN")
+webhook = config.get("SLACK", "WEBHOOK")
 
 imap_server = "imap.gmail.com"
 smtp_server = "smtp.gmail.com"
 addr = config.get("EMAIL", "ADDRESS")
 pwd = config.get("EMAIL", "PASSWORD")
-bot = WebClient(token=BOT_TOKEN)
-readonly_state = False
+readonly_state = True
 
 def fetch_unread():
 
@@ -99,81 +99,90 @@ def post_unread():
     unread_messages = fetch_unread()
     if unread_messages:
         for msg in unread_messages:
-            res = bot.chat_postMessage(
-                channel = email_channel,
-                text = "",
-                blocks = [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "*You have a new email:inbox_tray:*"
-                        }
-                    },
-                    {
-                        "type": "section",
-                        "fields": [
-                            {
-                                "type": "mrkdwn",
-                                "text": f"*From:*\n{msg['from']}"
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": f"*To:*\n{msg['to']}"
-                            },
-                            {
-                                "type": "mrkdwn",
-                                "text": f"*Subject:*\n{msg['Subject']}"
-                            }
-                        ]
-                    },
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": msg["body"]
-                        }
-                    },
-                    {
-                        "type": "actions",
-                        "elements": [
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "emoji": True,
-                                    "text": ":bust_in_silhouette:Assign"
-                                },
-                                "style": "primary",
-                                "value": "assign_email"
-                            },
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "emoji": True,
-                                    "text": ":email:Reply"
-                                },
-                                "value": "reply"
-                            },
-                            {
-                                "type": "button",
-                                "text": {
-                                    "type": "plain_text",
-                                    "emoji": True,
-                                    "text": ":white_check_mark:Complete"
-                                },
-                                "style": "primary",
-                                "value": "email_completed"
-                            }
-                        ]
-                    },
-                ]
+            
+            slack_data = {
+              'text':"New Email",
+              'blocks': [
+                  {
+                      "type": "section",
+                      "text": {
+                          "type": "mrkdwn",
+                          "text": "*You have a new email:inbox_tray:*"
+                      }
+                  },
+                  {
+                      "type": "section",
+                      "fields": [
+                          {
+                              "type": "mrkdwn",
+                              "text": f"*From:*\n{msg['from']}"
+                          },
+                          {
+                              "type": "mrkdwn",
+                              "text": f"*To:*\n{msg['to']}"
+                          },
+                          {
+                              "type": "mrkdwn",
+                              "text": f"*Subject:*\n{msg['Subject']}"
+                          }
+                      ]
+                  },
+                  {
+                      "type": "section",
+                      "text": {
+                          "type": "mrkdwn",
+                          "text": msg["body"]
+                      }
+                  },
+                  {
+                      "type": "actions",
+                      "elements": [
+                          {
+                              "type": "button",
+                              "text": {
+                                  "type": "plain_text",
+                                  "emoji": True,
+                                  "text": ":bust_in_silhouette:Assign"
+                              },
+                              "style": "primary",
+                              "value": "assign_email"
+                          },
+                          {
+                              "type": "button",
+                              "text": {
+                                  "type": "plain_text",
+                                  "emoji": True,
+                                  "text": ":email:Reply"
+                              },
+                              "value": "reply"
+                          },
+                          {
+                              "type": "button",
+                              "text": {
+                                  "type": "plain_text",
+                                  "emoji": True,
+                                  "text": ":white_check_mark:Complete"
+                              },
+                              "style": "primary",
+                              "value": "email_completed"
+                          }
+                      ]
+                  },
+              ]
+            }
+            response = requests.post(
+              webhook, data=json.dumps(slack_data),
+              headers={'Content-Type': 'application/json'}
             )
+            if response.status_code != 200:
+                raise ValueError(
+                    'Request to slack returned an error %s, the response is:\n%s'
+                    % (response.status_code, response.text)
+                )
 
-while True:
-  post_unread()
-  time.sleep(300)
+# while True:
+#   post_unread()
+#   time.sleep(300)
 # reply(addr, "caleb.njiiri@gmail.com","tyui", "Netflix is a joke" )
 # pprint(fetch_unread())
 
